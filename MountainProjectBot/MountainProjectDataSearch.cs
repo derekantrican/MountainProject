2de730f4f358b4e64fd534e80f9f42f8a5b83ea5
derekantrican <derekantrican@gmail.com>
2019-04-30 08:06:41 -0700

    Rearranging of bot code for clarity

---
+++ MountainProjectBot/MountainProjectDataSearch.cs
@@ -0,0 +1,150 @@
+﻿using MountainProjectModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MountainProjectBot
+{
+    public static class MountainProjectDataSearch
+    {
+        public static List<Area> DestAreas = new List<Area>();
+
+        public static void InitMountainProjectData(string xmlPath)
+        {
+            Console.WriteLine("Deserializing info from MountainProject");
+
+            FileStream fileStream = new FileStream(xmlPath, FileMode.Open);
+            XmlSerializer xmlDeserializer = new XmlSerializer(typeof(List<Area>));
+            DestAreas = (List<Area>)xmlDeserializer.Deserialize(fileStream);
+
+            if (DestAreas.Count == 0)
+            {
+                Console.WriteLine("Problem deserializing MountainProject info");
+                Environment.Exit(13); //Invalid data
+            }
+
+            Console.WriteLine("MountainProject Info deserialized successfully");
+        }
+
+        public static string SearchMountainProject(string searchText)
+        {
+            Console.WriteLine("Getting info from MountainProject");
+            Stopwatch searchStopwatch = Stopwatch.StartNew();
+
+            MPObject result = DeepSearch(searchText, DestAreas);
+            if (result == null)
+                return null;
+
+            Console.WriteLine($"Info retrieved from MountainProject (found in {searchStopwatch.ElapsedMilliseconds} ms)");
+
+            return GetFormattedString(result);
+        }
+
+        private static string GetFormattedString(MPObject inputMountainProjectObject)
+        {
+            string result = "I found the following info:\n\n";
+
+            if (inputMountainProjectObject is Area)
+            {
+                Area inputArea = inputMountainProjectObject as Area;
+                result += $"{inputArea.Name} [{inputArea.Statistics}]\n" +
+                         inputArea.URL;
+
+                //Todo: additional info to add
+                // - located in {destArea}
+                // - popular routes
+            }
+            else if (inputMountainProjectObject is Route)
+            {
+                Route inputRoute = inputMountainProjectObject as Route;
+                result += $"{inputRoute.Name} [{inputRoute.Type} {inputRoute.Grade},";
+
+                if (!string.IsNullOrEmpty(inputRoute.AdditionalInfo))
+                    result += " " + inputRoute.AdditionalInfo;
+
+                result += "]\n";
+                result += inputRoute.URL;
+
+                //Todo: additional info to add
+                // - located in {destArea}
+                // - # of bolts (if sport)
+            }
+
+            return result;
+        }
+
+        private static MPObject DeepSearch(string input, List<Area> destAreas)
+        {
+            MPObject matchedObject = null;
+            foreach (Area destArea in destAreas)
+            {
+                if (input.ToLower().Contains(destArea.Name.ToLower()))
+                {
+                    //If we're matching the name of a destArea (eg a State), we'll assume that the route/area is within that state
+                    //(eg routes named "Sweet Home Alabama"). So instead of returning the destArea, we'll return a search on the
+                    //state's subareas
+                    matchedObject = SearchSubAreasForMatch(input, destArea.SubAreas);
+                    if (matchedObject != null)
+                        return matchedObject;
+                }
+
+                if (destArea.SubAreas != null &&
+                    destArea.SubAreas.Count() > 0)
+                {
+                    matchedObject = SearchSubAreasForMatch(input, destArea.SubAreas);
+                    if (matchedObject != null)
+                        return matchedObject;
+                }
+            }
+
+            return matchedObject;
+        }
+
+        private static MPObject SearchSubAreasForMatch(string input, List<Area> subAreas)
+        {
+            MPObject matchedObject = null;
+
+            foreach (Area subDestArea in subAreas)
+            {
+                if (input.Equals(subDestArea.Name, StringComparison.InvariantCultureIgnoreCase))
+                    return subDestArea;
+
+                if (subDestArea.SubAreas != null &&
+                    subDestArea.SubAreas.Count() > 0)
+                {
+                    matchedObject = SearchSubAreasForMatch(input, subDestArea.SubAreas);
+                    if (matchedObject != null)
+                        return matchedObject;
+                }
+
+                if (subDestArea.Routes != null &&
+                    subDestArea.Routes.Count() > 0)
+                {
+                    matchedObject = SearchRoutes(input, subDestArea.Routes);
+                    if (matchedObject != null)
+                        return matchedObject;
+                }
+            }
+
+            return matchedObject;
+        }
+
+        private static MPObject SearchRoutes(string input, List<Route> routes)
+        {
+            MPObject matchedObject = null;
+
+            foreach (Route route in routes)
+            {
+                if (input.Equals(route.Name, StringComparison.InvariantCultureIgnoreCase))
+                    return route;
+            }
+
+            return matchedObject;
+        }
+    }
+}
--- MountainProjectBot/Program.cs
+++ MountainProjectBot/Program.cs
@@ -23,12 +23,11 @@ namespace MountainProjectBot
 
         static Reddit redditService;
         static Subreddit subReddit;
-        static List<Area> MountainProjectDestAreas = new List<Area>();
 
         static void Main(string[] args)
         {
             CheckRequiredFiles();
-            InitMountainProjectData();
+            MountainProjectDataSearch.InitMountainProjectData(XMLPATH);
             AuthReddit().Wait();
             DoBotLoop().Wait();
         }
@@ -61,23 +60,6 @@ namespace MountainProjectBot
             subReddit = await redditService.GetSubredditAsync(SUBREDDITNAME);
 
             Console.WriteLine("Reddit authed successfully");
-        }
-
-        private static void InitMountainProjectData()
-        {
-            Console.WriteLine("Deserializing info from MountainProject");
-
-            FileStream fileStream = new FileStream(XMLPATH, FileMode.Open);
-            XmlSerializer xmlDeserializer = new XmlSerializer(typeof(List<Area>));
-            MountainProjectDestAreas = (List<Area>)xmlDeserializer.Deserialize(fileStream);
-
-            if (MountainProjectDestAreas.Count == 0)
-            {
-                Console.WriteLine("Problem deserializing MountainProject info");
-                Environment.Exit(13); //Invalid data
-            }
-
-            Console.WriteLine("MountainProject Info deserialized successfully");
         }
 
         private static WebAgent GetWebAgentCredentialsFromFile()
@@ -128,7 +110,7 @@ namespace MountainProjectBot
             Console.WriteLine("Getting reply for comment");
 
             string queryText = replyTo.Body.Replace(BOTKEYWORD, "").Trim();
-            string replyText = SearchMountainProject(queryText);
+            string replyText = MountainProjectDataSearch.SearchMountainProject(queryText);
             if (string.IsNullOrEmpty(replyText))
                 replyText = $"I could not find anything for \"{queryText}\". Please use the Feedback button below if you think this is a bug";
 
@@ -138,122 +120,6 @@ namespace MountainProjectBot
             replyText += CreateMDLink("GitHub", "https://github.com/derekantrican/MountainProjectScraper") + " | ";
 
             return replyText;
-        }
-
-        private static string GetFormattedString(MPObject inputMountainProjectObject)
-        {
-            string result = "I found the following info:\n\n";
-
-            if (inputMountainProjectObject is Area)
-            {
-                Area inputArea = inputMountainProjectObject as Area;
-                result += $"{inputArea.Name} [{inputArea.Statistics}]\n" +
-                         inputArea.URL;
-
-                //Todo: additional info to add
-                // - located in {destArea}
-                // - popular routes
-            }
-            else if (inputMountainProjectObject is Route)
-            {
-                Route inputRoute = inputMountainProjectObject as Route;
-                result += $"{inputRoute.Name} [{inputRoute.Type} {inputRoute.Grade},";
-
-                if (!string.IsNullOrEmpty(inputRoute.AdditionalInfo))
-                    result += " " + inputRoute.AdditionalInfo;
-
-                result += "]\n";
-                result += inputRoute.URL;
-
-                //Todo: additional info to add
-                // - located in {destArea}
-                // - # of bolts (if sport)
-            }
-
-            return result;
-        }
-
-        private static string SearchMountainProject(string searchText)
-        {
-            Console.WriteLine("Getting info from MountainProject");
-            Stopwatch searchStopwatch = Stopwatch.StartNew();
-
-            MPObject result = DeepSearch(searchText, MountainProjectDestAreas);
-            if (result == null)
-                return null;
-
-            Console.WriteLine($"Info retrieved from MountainProject (found in {searchStopwatch.ElapsedMilliseconds} ms)");
-
-            return GetFormattedString(result);
-        }
-
-        private static MPObject DeepSearch(string input, List<Area> destAreas)
-        {
-            MPObject matchedObject = null;
-            foreach (Area destArea in destAreas)
-            {
-                if (input.ToLower().Contains(destArea.Name.ToLower()))
-                {
-                    //If we're matching the name of a destArea (eg a State), we'll assume that the route/area is within that state
-                    //(eg routes named "Sweet Home Alabama"). So instead of returning the destArea, we'll return a search on the
-                    //state's subareas
-                    matchedObject = SearchSubAreasForMatch(input, destArea.SubAreas);
-                    if (matchedObject != null)
-                        return matchedObject;
-                }
-
-                if (destArea.SubAreas != null &&
-                    destArea.SubAreas.Count() > 0)
-                {
-                    matchedObject = SearchSubAreasForMatch(input, destArea.SubAreas);
-                    if (matchedObject != null)
-                        return matchedObject;
-                }
-            }
-
-            return matchedObject;
-        }
-
-        private static MPObject SearchSubAreasForMatch(string input, List<Area> subAreas)
-        {
-            MPObject matchedObject = null;
-
-            foreach (Area subDestArea in subAreas)
-            {
-                if (input.Equals(subDestArea.Name, StringComparison.InvariantCultureIgnoreCase))
-                    return subDestArea;
-
-                if (subDestArea.SubAreas != null &&
-                    subDestArea.SubAreas.Count() > 0)
-                {
-                    matchedObject = SearchSubAreasForMatch(input, subDestArea.SubAreas);
-                    if (matchedObject != null)
-                        return matchedObject;
-                }
-
-                if (subDestArea.Routes != null &&
-                    subDestArea.Routes.Count() > 0)
-                {
-                    matchedObject = SearchRoutes(input, subDestArea.Routes);
-                    if (matchedObject != null)
-                        return matchedObject;
-                }
-            }
-
-            return matchedObject;
-        }
-
-        private static MPObject SearchRoutes(string input, List<Route> routes)
-        {
-            MPObject matchedObject = null;
-
-            foreach (Route route in routes)
-            {
-                if (input.Equals(route.Name, StringComparison.InvariantCultureIgnoreCase))
-                    return route;
-            }
-
-            return matchedObject;
         }
 
         private static List<Comment> RemoveAlreadyRepliedTo(List<Comment> comments)
