dea76e817c5f2d9ab9f1f2ca3fc19b5dcd47300f
derekantrican <derekantrican@gmail.com>
2019-05-09 05:51:28 -0700

    Upgraded unit tests

--- UnitTests/ParserTests.cs
+++ UnitTests/ParserTests.cs
@@ -78,12 +78,12 @@ namespace UnitTests
             List<Area> destAreas = Parsers.GetDestAreas();
             List<string> resultNames = destAreas.Select(p => p.Name).ToList();
 
-            CollectionAssert.AreEqual(expectedDestAreas, resultNames);
+            CollectionAssert.AreEqual(expectedDestAreas, resultNames); //Compare collections WITH order
         }
 
         [DataTestMethod]
-        [DataRow("/area/107605102/bankhead-forest-thompson-creek-trail", 2, 0)]
-        [DataRow("/area/108184422/deception-wall", 0, 17)]
+        [DataRow("/area/107605102/bankhead-forest-thompson-creek-trail", 2, 0)] //Subdest areas have subareas but no routes
+        [DataRow("/area/108184422/deception-wall", 0, 17)] //Walls have routes but no subareas
         public void TestSubAreaParse(string url, int expectedSubAreas, int expectedRoutes)
         {
             if (!url.Contains(Utilities.MPBASEURL))
@@ -101,11 +101,10 @@ namespace UnitTests
         }
 
         [DataTestMethod]
-        [DataRow("/route/111859673/side-dish", "Side Dish", new[] { Route.RouteType.Sport }, "5.10c", "50 ft")]
-        [DataRow("/route/109063052/geflugelfrikadelle", "Geflügelfrikadelle", new[] { Route.RouteType.Trad, Route.RouteType.Aid }, "5.12b/c", "40 ft")]
-        [DataRow("/route/116181996/13-above-the-night", "13 Above the Night", new[] { Route.RouteType.Trad, Route.RouteType.Mixed, Route.RouteType.Ice, Route.RouteType.Alpine }, "WI4 M5", "1000 ft, 5 pitches, Grade IV")]
-        [DataRow("/route/110425910/birds-of-a-feather", "Birds of a Feather", new[] { Route.RouteType.Sport, Route.RouteType.TopRope }, "5.8", "50 ft")]
-        public void TestRouteParse(string url, string expectedName, Route.RouteType[] expectedTypes, string expectedGrade, string expectedAddInfo)
+        [DataRow("/route/111859673/side-dish", "Side Dish")]
+        [DataRow("/route/109063052/geflugelfrikadelle", "Geflügelfrikadelle")] //Special characters
+        [DataRow("/route/112177605/no-rang-na-rang", "너랑나랑 (no-rang-na-rang)")] //Special characters
+        public void TestRouteNameParse(string url, string expectedName)
         {
             if (!url.Contains(Utilities.MPBASEURL))
                 url = Utilities.MPBASEURL + url;
@@ -114,9 +113,67 @@ namespace UnitTests
             Parsers.ParseRouteAsync(testRoute).Wait();
 
             Assert.AreEqual(expectedName, testRoute.Name);
-            CollectionAssert.AreEquivalent(expectedTypes, testRoute.Types);
+        }
+
+        [DataTestMethod]
+        [DataRow("/route/111859673/side-dish", new[] { Route.RouteType.Sport })]
+        [DataRow("/route/109063052/geflugelfrikadelle", new[] { Route.RouteType.Trad, Route.RouteType.Aid })]
+        [DataRow("/route/116181996/13-above-the-night", new[] { Route.RouteType.Trad, Route.RouteType.Mixed, Route.RouteType.Ice, Route.RouteType.Alpine })] //Many Types
+        [DataRow("/route/110425910/birds-of-a-feather", new[] { Route.RouteType.Sport, Route.RouteType.TopRope })] //Top Rope
+        public void TestRouteTypeParse(string url, Route.RouteType[] expectedTypes)
+        {
+            if (!url.Contains(Utilities.MPBASEURL))
+                url = Utilities.MPBASEURL + url;
+
+            Route testRoute = new Route() { URL = url };
+            Parsers.ParseRouteAsync(testRoute).Wait();
+
+            CollectionAssert.AreEquivalent(expectedTypes, testRoute.Types); //Compare collections WITHOUT order
+        }
+
+        [DataTestMethod]
+        [DataRow("/route/111859673/side-dish", "5.10c")]
+        [DataRow("/route/109063052/geflugelfrikadelle", "5.12b/c")] //Has a slash
+        [DataRow("/route/116181996/13-above-the-night", "WI4 M5")] //No YDS/Heuco present
+        public void TestRouteGradeParse(string url, string expectedGrade)
+        {
+            if (!url.Contains(Utilities.MPBASEURL))
+                url = Utilities.MPBASEURL + url;
+
+            Route testRoute = new Route() { URL = url };
+            Parsers.ParseRouteAsync(testRoute).Wait();
+
             Assert.AreEqual(expectedGrade, testRoute.Grade);
-            Assert.AreEqual(expectedAddInfo, testRoute.AdditionalInfo);
+        }
+
+        [DataTestMethod]
+        [DataRow("/route/109063052/geflugelfrikadelle", "40 ft")]
+        [DataRow("/route/116181996/13-above-the-night", "1000 ft, 5 pitches, Grade IV")] //Lots of additional info
+        [DataRow("/route/110425910/birds-of-a-feather", "50 ft")] //Had a weird (multiple comma) parsing issue before
+        public void TestRouteAdditionalInfoParse(string url, string expectedAdditionalInfo)
+        {
+            if (!url.Contains(Utilities.MPBASEURL))
+                url = Utilities.MPBASEURL + url;
+
+            Route testRoute = new Route() { URL = url };
+            Parsers.ParseRouteAsync(testRoute).Wait();
+
+            Assert.AreEqual(expectedAdditionalInfo, testRoute.AdditionalInfo);
+        }
+
+        [DataTestMethod] //https://stackoverflow.com/a/54296734/2246411
+        [DataRow("/area/105791955/exit-38-deception-crags", new object[] { new[] { "/route-guide", "/area/105708966/washington", "/area/108471374/central-west-cascades-seattle", "/area/108471684/north-bend-vicinity", "/area/114278624/exit-38" } })] //Area
+        [DataRow("/route/112177605/no-rang-na-rang", new object[] { new[] { "/route-guide", "/area/105907743/international", "/area/106661515/asia", "/area/106225629/south-korea", "/area/112177596/gwanaksankwanaksan-cha-oon-am-san-sleeping-rock-mountain" } })] //Route
+        public void TestParentParse(string url, object[] expectedParentUrls)
+        {
+            if (!url.Contains(Utilities.MPBASEURL))
+                url = Utilities.MPBASEURL + url;
+
+            List<string> parents = Parsers.PopulateParentUrls(Utilities.GetHtmlDoc(url));
+            for (int i = 0; i < parents.Count; i++)
+                parents[i] = parents[i].Replace(Utilities.MPBASEURL, "");
+
+            CollectionAssert.AreEqual(expectedParentUrls, parents); //Compare collections WITH order
         }
 
         [DataTestMethod]
