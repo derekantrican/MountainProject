cf7411193e01bf3858f65347b81bcd41b7834668
Derek Antrican <derekantrican@gmail.com>
2019-08-11 10:09:35 -0700

    Sanitize strings before applying markdown (eg route names that start with an asterisk)

--- MountainProjectBot/Markdown.cs
+++ MountainProjectBot/Markdown.cs
@@ -5,17 +5,17 @@ namespace MountainProjectBot
     {
         public static string Bold(string textToBold)
         {
-            return $"**{textToBold}**";
+            return $"**{textToBold.Trim('*')}**";
         }
 
         public static string Link(string linkText, string linkUrl)
         {
-            return $"[{linkText}]({linkUrl})";
+            return $"[{linkText.Trim('[', ']')}]({linkUrl.Trim('(', ')')})";
         }
 
         public static string InlineCode(string text)
         {
-            return $"`{text}`";
+            return $"`{text.Trim('`')}`";
         }
 
         public static string HRule { get { return "\n\n-----\n\n"; } }
