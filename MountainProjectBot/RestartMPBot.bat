6f7dbb0e4fd53faacca543fabdbd3479bc5220b0
Derek Antrican <derekantrican@gmail.com>
2020-01-30 09:04:56 -0800

    Updated Restart bat file to not output errors if process is not running

--- MountainProjectBot/RestartMPBot.bat
+++ MountainProjectBot/RestartMPBot.bat
@@ -1,3 +1,11 @@
-@taskkill /f /im MountainProjectBot.exe >nul
+@ECHO OFF
+
+REM Kill MountainProjectBot.exe if it is running
+tasklist /FI "IMAGENAME eq MountainProjectBot.exe" 2>NUL | find /I /N "MountainProjectBot.exe">NUL
+if "%ERRORLEVEL%"=="0" taskkill /f /im MountainProjectBot.exe
+
+REM Wait 3 seconds
 @timeout /t 3 /nobreak >nul
+
+REM Start up MountainProjectBot.exe again
 @start bin\MountainProjectBot.exe xmlpath=..\MountainProjectDBBuilder\bin\MountainProjectAreas.xml repliedto=bin\RepliedTo.txt repliedtoposts=bin\RepliedToPosts.txt blacklisted=bin\BlacklistedUsers.txt >nul
