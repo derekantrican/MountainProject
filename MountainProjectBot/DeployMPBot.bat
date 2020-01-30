c2d6116004079a849b04b7a8ca075b532bd7c757
Derek Antrican <derekantrican@gmail.com>
2020-01-30 08:53:59 -0800

    Updated deploy bat

--- MountainProjectBot/DeployMPBot.bat
+++ MountainProjectBot/DeployMPBot.bat
@@ -4,10 +4,10 @@ REM pull latest code from github
 git pull
 
 REM Build sln
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"  ..\MountainProject.sln /p:Configuration=Debug /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"  ..\MountainProject.sln /p:Configuration=Debug
 
 REM Run bot to update the replied txt files
 bin\MountainProjectBot.exe dryrun xmlpath=..\MountainProjectDBBuilder\bin\MountainProjectAreas.xml repliedto=bin\RepliedTo.txt repliedtoposts=bin\RepliedToPosts.txt blacklisted=bin\BlacklistedUsers.txt
 
 REM Start bot normally
-bin\MountainProjectBot.exe xmlpath=..\MountainProjectDBBuilder\bin\MountainProjectAreas.xml repliedto=bin\RepliedTo.txt repliedtoposts=bin\RepliedToPosts.txt blacklisted=bin\BlacklistedUsers.txt
+RestartMPBot.bat
