43ec6283b59095549b90c4346e98116e56bca0ed
Derek Antrican <derekantrican@gmail.com>
2020-11-17 08:19:19 -0800

    Changed line count for recent server lines

--- MountainProjectBot/Program.cs
+++ MountainProjectBot/Program.cs
@@ -119,7 +119,7 @@ namespace MountainProjectBot
                     {
                         string serverActivity = "";
                         string[] serverLines = BotUtilities.ApprovalServer.Activity.ToString().Split('\n');
-                        foreach (string line in serverLines.Skip(Math.Max(0, serverLines.Count() - 50)))
+                        foreach (string line in serverLines.Skip(Math.Max(0, serverLines.Count() - 25)))
                             serverActivity += $"{line}\n";
 
                         BotUtilities.SendDiscordMessage($"Recent server lines:\n\n{serverActivity}");
