7462406e28525d7cd568c5185944d28743427b0c
Derek Antrican <derekantrican@gmail.com>
2021-03-09 16:19:22 -0800

    Updated deploy bat

--- MountainProjectBot/DeployMPBot.bat
+++ MountainProjectBot/DeployMPBot.bat
@@ -5,10 +5,11 @@ git fetch --all
 git reset --hard origin/master
 
 REM restore nuget packages
-dotnet restore
+nuget restore ..\
 
 REM Build sln
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"  ..\MountainProject.sln /p:Configuration=Debug
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM Run bot to update the replied txt files
 bin\MountainProjectBot.exe dryrun xmlpath=..\MountainProjectDBBuilder\bin\MountainProjectAreas.xml repliedto=bin\RepliedTo.txt repliedtoposts=bin\RepliedToPosts.txt blacklisted=bin\BlacklistedUsers.txt
