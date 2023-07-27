bfc182845bcedf24c2db86c3ceed905ae4351fa9
Derek Antrican <derekantrican@gmail.com>
2023-07-26 20:00:22 -0700

    Updated unit test expected values

--- UnitTests/ParserTests.cs
+++ UnitTests/ParserTests.cs
@@ -122,7 +122,7 @@ namespace UnitTests
         }
 
         [DataTestMethod]
-        [DataRow("/area/105841134/red-river-gorge", new object[] { new[] { "/route/105860741/roadside-attraction", "/route/106405603/crack-attack", "/route/105868000/rock-wars" } })] //Some popular routes
+        [DataRow("/area/105841134/red-river-gorge", new object[] { new[] { "/route/105860741/roadside-attraction", "/route/106125099/27-years-of-climbing", "/route/106085043/send-me-on-my-way" } })] //Some popular routes
         [DataRow("/area/107605102/bankhead-forest-thompson-creek-trail", new object[] { new string[0] })] //No popular routes listed
         public void TestAreaPopularClimbsParse(string url, object[] expectedPopClimbs)
         {
